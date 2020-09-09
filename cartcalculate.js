11d1e6b17dd38610fd24e6375b3039dbe17a5bcc
Vageesha Krishna <53399088+vageeshakrishna@users.noreply.github.com>
2020-09-09 20:28:13 +0530

    added files

---
+++ cartcalculate.js
@@ -0,0 +1,16 @@
+const plus = document.querySelector(".add");
+const less = document.querySelector(".less");
+const itemvalue = document.querySelector(".cart-item-value");
+
+var itemcount = parseInt(itemvalue.innerHTML);
+plus.addEventListener("click", () => {
+  itemcount++;
+  itemvalue.innerHTML = itemcount;
+});
+
+less.addEventListener("click", () => {
+  if (itemcount > 0) {
+    itemcount--;
+    itemvalue.innerHTML = itemcount;
+  }
+});
